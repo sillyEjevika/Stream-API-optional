1b3f585c65f3961e5983266f2c687167111d6d56
sillyEjevika <Doroteia_Vika@mail.ru>
2023-03-12 11:48:39 +0300

    ссс

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+package ru.catunderglue;
+import java.util.Comparator;
+import java.util.List;
+import java.util.function.BiConsumer;
+import java.util.stream.Stream;
+public class Main {
+    public static void main(String[] args) {
